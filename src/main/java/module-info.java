100ca28357c6e91676932d40ea888c488a607e49
Mark Bowman <mbowman@lco.global>
2023-03-06 23:41:57 +0000

    Fix module exports

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,23 @@
 module jade {
+    exports jade;
+    exports jade.content;
+    exports jade.content.lang;
+    exports jade.content.lang.sl;
+    exports jade.content.onto.basic;
+    exports jade.core;
+    exports jade.core.behaviours;
+    exports jade.core.messaging;
+    exports jade.gui;
+    exports jade.proto;
+    exports jade.lang.acl;
+    exports jade.domain;
+    exports jade.domain.FIPAAgentManagement;
+    exports jade.domain.introspection;
+    exports jade.domain.JADEAgentManagement;
+    exports jade.util.leap;
+    exports jade.wrapper;
+    exports jade.wrapper.gateway;
+
     requires java.desktop;
     requires java.logging;
     requires org.apache.commons.codec;
@@ -6,5 +25,4 @@ module jade {
     requires java.sql;
     requires glassfish.corba.omgapi;
     requires java.management;
-    exports jade;
 }
