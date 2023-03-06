c5e921e7298ea6189330624ad5fd8fc7760596b8
Mark Bowman <mbowman@lco.global>
2023-03-06 21:22:42 +0000

    Minimum changes to compile under java 17

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module jade {
+    requires java.desktop;
+    requires java.logging;
+    requires org.apache.commons.codec;
+    requires java.rmi;
+    requires java.sql;
+    requires glassfish.corba.omgapi;
+    requires java.management;
+    exports jade;
+}
